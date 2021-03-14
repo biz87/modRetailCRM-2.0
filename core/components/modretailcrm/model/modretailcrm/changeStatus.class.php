769963bc09df05baf1a52222c98165a434dd23c3
Nikolay Savin <info@megawebs.kz>
2021-03-14 15:41:46 +0600

    2.11.1

--- core/components/modretailcrm/model/modretailcrm/changeStatus.class.php
+++ core/components/modretailcrm/model/modretailcrm/changeStatus.class.php
@@ -15,37 +15,37 @@ class ChangeStatus
     public function __construct(modRetailCrm & $modretailcrm)
     {
         $this->modretailcrm = &$modretailcrm;
-
         $this->modx = &$modretailcrm->modx;
     }
 
-    public function msOnChangeOrderStatus($status, $order)
+    /**
+     * @param int $status_id
+     * @param msOrder $order
+     */
+    public function msOnChangeOrderStatus($status_id, $order)
     {
-        $modx =& $this->modx;
-
-        $modRetailCrm =& $this->modretailcrm;
-
-        if ($status === 1) {
+        if ($status_id === 1) {
             return;
         }
-        $syncStatuses = $modx->getOption('modretailcrm_sync_statuses');
+
+        $syncStatuses = $this->modx->getOption('modretailcrm_sync_statuses');
         if (!empty($syncStatuses)) {
             $syncStatuses = array_map("trim", explode(',', $syncStatuses));
             if (is_array($syncStatuses) && count($syncStatuses) > 0) {
-                $statusObj = $modx->getObject('msOrderStatus', array('id' => $status));
-                if ($statusObj) {
-                    $retailcrm_status_code = $statusObj->get('retailcrm_status_code');
-                    if (!empty($retailcrm_status_code) && in_array($status, $syncStatuses)) {
+                $msOrderStatus = $this->modx->getObject('msOrderStatus', array('id' => $status_id));
+                if ($msOrderStatus) {
+                    $retailcrm_status_code = $msOrderStatus->get('retailcrm_status_code');
+                    if (!empty($retailcrm_status_code) && in_array($status_id, $syncStatuses)) {
                         $orderData = array();
-                        $orderData['externalId'] = str_replace('/', '-', $order->num);
+                        $orderData['externalId'] = str_replace('/', '-', $order->get('num'));
                         $orderData['status'] = $retailcrm_status_code;
-                        $response = $modRetailCrm->request->ordersEdit($orderData, $by = 'externalId');
-                        if ($modx->getOption('modretailcrm_log')) {
-                            $modx->log(modX::LOG_LEVEL_ERROR, '[ModRetailCrm] - Результат отправки статуса заказа ' . print_r($response, 1));
+                        $response = $this->modretailcrm->request->ordersEdit($orderData, $by = 'externalId');
+                        if ($this->modx->getOption('modretailcrm_log')) {
+                            $this->modx->log(modX::LOG_LEVEL_ERROR, '[ModRetailCrm] - Результат отправки статуса заказа ' . print_r($response, 1));
                         }
 
                         //Если Статус оплачено - меняем статус платежа
-                        if ($status = 2) {
+                        if ($status_id == 2) {
                             $externalId = $orderData['externalId'];
                             $this->ordersPaymentEdit($order, $externalId);
                         }
@@ -56,20 +56,19 @@ class ChangeStatus
     }
 
 
+    /**
+     * @param msOrder $order
+     * @param integer|string $externalId
+     */
     public function ordersPaymentEdit($order, $externalId)
     {
-        $modx =& $this->modx;
-
-        $modRetailCrm =& $this->modretailcrm;
-
-
         //Ищем номер платежа
-        $orders = $modRetailCrm->request->ordersList(array('externalIds' => [$externalId]), 1, 20);
-        if ($modx->getOption('modretailcrm_log')) {
-            $modx->log(modX::LOG_LEVEL_ERROR, '[ModRetailCrm] -  Ищу данные о номере платежа ' . print_r($orders, 1));
+        $orders = $this->modretailcrm->request->ordersList(array('externalIds' => [$externalId]), 1, 20);
+        if ($this->modx->getOption('modretailcrm_log')) {
+            $this->modx->log(modX::LOG_LEVEL_ERROR, '[ModRetailCrm] -  Ищу данные о номере платежа ' . print_r($orders, 1));
         }
         $paymentId = 0;
-        if (count($orders) > 0) {
+        if (count($orders['orders']) > 0) {
             foreach ($orders['orders'][0]['payments'] as $payment) {
                 $paymentId = $payment['id'];
             }
@@ -78,13 +77,13 @@ class ChangeStatus
         if ($paymentId > 0) {
             $payment = array();
             $payment['id'] = $paymentId;
-            $payment['payment'] = $order->cost;
-            $payment['paidAt'] = $order->updatedon;
+            $payment['payment'] = $order->get('cost');
+            $payment['paidAt'] = $order->get('updatedon');
             $payment['status'] = 'paid';
 
-            $paymentEdit = $modRetailCrm->request->ordersPaymentEdit($payment, 'id');
-            if ($modx->getOption('modretailcrm_log')) {
-                $modx->log(modX::LOG_LEVEL_ERROR, '[ModRetailCrm] -  Обновляю статус платежа ' . print_r($paymentEdit, 1));
+            $paymentEdit = $this->modretailcrm->request->ordersPaymentEdit($payment, 'id');
+            if ($this->modx->getOption('modretailcrm_log')) {
+                $this->modx->log(modX::LOG_LEVEL_ERROR, '[ModRetailCrm] -  Обновляю статус платежа ' . print_r($paymentEdit, 1));
             }
 
         }
